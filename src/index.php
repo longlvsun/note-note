a50d8e43c0e4d8d79c8b97212f83ff3cf3120b2c
Vo Luong Bang <voluongbang@Vos-MacBook-Air.local>
2022-07-29 09:34:32 +0700

    create home page and login

--- src/index.php
+++ src/index.php
@@ -1,3 +1,28 @@
-<?php
-  echo "Hello";
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <!-- Header -->
+    <?php include "./inc/header.php"; ?>
+    <!--Body container-->
+    <div class="height-100 bg-light">
+        <h4>Main Components</h4>
+    </div>
+
+    <!-- Footer -->
+    <?php include "./"; ?>
+
+</body>
+<script type='text/javascript' src="./js//main.js"></script>
+<script type='text/javascript' src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+<script type='text/javascript' src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+</html>
